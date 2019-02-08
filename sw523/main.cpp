25f0682fb564823ceefbfdfd90c1ae8fd8e4dcc4
maxinyu <xm546@nyu.edu>
2019-02-08 16:56:01 -0500

    Initial Commit

---
+++ sw523/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  sw523
+//
+//  Created by maxinyu on 2019/2/8.
+//  Copyright © 2019年 maxinyu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
