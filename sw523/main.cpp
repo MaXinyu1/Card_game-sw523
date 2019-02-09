5fbf431ad145054cad06c9edb58aecbf50fb0435
maxinyu <xm546@nyu.edu>
2019-02-09 17:07:28 -0500

    card game-2019.2.9

--- sw523/main.cpp
+++ sw523/main.cpp
@@ -6,10 +6,56 @@
 //  Copyright © 2019年 maxinyu. All rights reserved.
 //
 
-#include <iostream>
+/*
+ 1.UI
+ 2.multithreading server (different user different server)
+ */
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+/***************SW523 Game Rules***************/
+/*
+ 1.press 5 to skip
+ 2.press 9 to finish input
+ 3. 7 > 0 > 5 > 2 > 3 > 1 > 13 > 12 > 11 > 10 > 9 > 8 > 6 > 4
+ 4. 10 and 13 are credits
+ 5. the person who got the most credit wins
+ */
+ 
+#include <iostream>
+#include <vector>
+#include "cards.hpp"
+#include "player.hpp"
+using namespace std;
+
+int main() {
+    //create player
+    int player_num;
+    cout<<"How many players: ";
+    cin>>player_num;
+    
+    vector<player*> p;
+    string name, sex;
+    int level, beans;
+    
+    for(int i = 0; i < player_num; i++) {
+        cin>>name>>sex>>level>>beans;
+        player *newp = new player(name,sex,level,beans);
+        p.emplace_back(newp);
+    }
+    
+    
+    //create cards
+    cards *c = new cards;
+    int card_num;
+    cout<<"How many card: ";
+    cin>>card_num;
+    c->createCards(card_num);
+    
+    //shuffle cards
+    c->shuffleCards();
+    
+    //deal cards
+    c->dealCards(p);
+    
+    //play game
+    c->game(p);
 }
